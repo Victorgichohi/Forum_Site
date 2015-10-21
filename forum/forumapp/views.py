1a043a77192ad482c19cad972717eeea331af9b3
victorgichohi <victorgichohi48@gmail.com>
2015-10-21 18:06:03 +0300

    added a list view model in the views file which displays the main forums

--- forum/forumapp/views.py
+++ forum/forumapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from dbe.mcbv.list_custom import ListView, ListRelated
+
+# this is just a simple list view displaying the main listing of the forums forums
+class Main(ListView):
+    list_model    = Forum
+    template_name = "forum/list.html"
